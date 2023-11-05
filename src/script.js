290b919f2b13f68abb82313908cb8ec1876078de
Christina Ashley Cruz <christina.ash.cruz@gmail.com>
2023-11-05 14:10:11 -0800

    fixed valid error message in js

--- src/script.js
+++ src/script.js
@@ -1,15 +1,40 @@
+// email input element
 const inputElement = document.querySelector("#email");
+// error message block element
 const errorMessage = document.querySelector("#error-message");
 
 // regex pattern
 const regexPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+// create event listener for input element and to run when input value changes
 inputElement.addEventListener("input", function () {
-  if (!regexPattern.test(inputElement.value)) {
-    errorMessage.classList.remove("hidden");
+  // check whether element is active and in focus and value is not blank
+  if (
+    inputElement == document.activeElement &&
+    inputElement.value != "" &&
+    !regexPattern.test(inputElement.value)
+  ) {
+    // error message appear and remove hidden class
     errorMessage.classList.add("block");
+    errorMessage.classList.remove("hidden");
+    // input classes to remove and add
+    inputElement.classList.remove("border-dark-slate-gray");
+    inputElement.classList.add(
+      "border-tomato",
+      "ring-tomato",
+      "text-tomato",
+      "bg-tomato",
+    );
   } else {
+    // hide error message with removing block class and adding hidden class
     errorMessage.classList.remove("block");
     errorMessage.classList.add("hidden");
+    // remove previous input classes from line 20
+    inputElement.classList.remove(
+      "border-tomato",
+      "ring-tomato",
+      "text-tomato",
+      "bg-tomato",
+    );
   }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     colors: {
       tomato: "hsl(4, 100%, 67%)",
